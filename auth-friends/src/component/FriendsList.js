7416967da7d8a934b39850d5e4c4984e6480cf90
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-25 20:28:44 -0500

    created friends list cards

--- auth-friends/src/component/FriendsList.js
+++ auth-friends/src/component/FriendsList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {axiosWithAuth} from '../utils/axiosWithAuth';
+import AddFriends from './AddFriends';
+import Paper from '@material-ui/core/Paper';
+
 
 class FriendsList extends Component {
     state = {
@@ -27,7 +30,16 @@ class FriendsList extends Component {
     render() {
         return (
             <div>
-                Logged in!
+                <AddFriends />
+                <div className='friend-list-container'>
+                    {this.state.friends.map(friend => 
+                        <Paper elevation={3} className='friends' >
+                            <h2>Name: {friend.name}</h2>
+                            <p>Age: {friend.age}</p>
+                            <p>Email: {friend.email}</p>
+                        </Paper>
+                    )}
+                </div>
             </div>
         );
     }
