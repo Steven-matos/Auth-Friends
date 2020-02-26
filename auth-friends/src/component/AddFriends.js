6bec22ee310c7837d696306bd309b2099eee0a48
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-02-25 20:29:11 -0500

    fixed bug on add friends form

--- auth-friends/src/component/AddFriends.js
+++ auth-friends/src/component/AddFriends.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class AddFriends extends Component {
     state = {
@@ -13,8 +15,10 @@ class AddFriends extends Component {
 
     HandleChange = e => {
         this.setState({
+            friend: {
             ...this.state.friend,     
             [e.target.name]: e.target.value
+            }
         })
     }
 
@@ -26,12 +30,20 @@ class AddFriends extends Component {
     }
 
     render() {
+        console.log(this.state.friend)
         return (
-            <form noValidate autoComplete="off" onSubmit={this.HandleSubmit}>
-                <TextField id="filled-basic" name='name' label="Name" variant="filled" onChange={this.HandleChange}/>
-                <TextField id="filled-basic" name='age' label="Age" variant="filled" onChange={this.HandleChange}/>
-                <TextField id="filled-basic" name='email' label="Email" type='email' variant="filled" onChange={this.HandleChange}/>
-            </form>
+            <div className='addfriend-container'>
+                <form className='addfriend-form' noValidate autoComplete="off" onSubmit={this.HandleSubmit}>
+                    <TextField id="filled-basic" name='name' value={this.state.friend.name} label="Name" variant="filled" onChange={this.HandleChange}/>
+                    <TextField id="filled-basic" name='age' value={this.state.friend.age} label="Age" variant="filled" onChange={this.HandleChange}/>
+                    <TextField id="filled-basic" name='email' value={this.state.friend.email} label="Email" type='email' variant="filled" onChange={this.HandleChange}/>
+                    <div className='addfriend-submit'>
+                        <Button  type='submit' variant="contained" color="primary">
+                            Submit
+                        </Button>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
